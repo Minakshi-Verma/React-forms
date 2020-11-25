9c800c84e3c7b41a6538b5a7fdb38701ba502bae
Minakshi Verma <mvedwan@gmail.com>
2020-11-25 01:48:12 -0500

    Validation is added to all the inputs

--- react-forms/src/StandardForm/Stdform.js
+++ react-forms/src/StandardForm/Stdform.js
@@ -12,6 +12,56 @@ const [form, setForm] =useState({
 
 const {username, userNameValid, email, emailValid, password, passwordValid, repeatPassword, repeatPasswordValid, formValid, errMsg} = form
 
+//Input validation
+
+//username validation
+const userValidation = () =>{
+    userNameValid= true
+    errMsg={...errMsg}
+    if(username.length<6 || username.length>15){
+        userNameValid= false
+        errMsg.username = "Username should have 6-15 character"
+    }
+    setForm({userNameValid, errMsg})
+}
+
+//email validation
+const emailValidation =()=>{
+    emailValid=true
+    errMsg={...errMsg}
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        emailValid=false;
+        errMsg.email={"Invalid email format"}
+    }
+    setForm(emailValid, errMsg)
+}
+
+//password validation
+
+const passwordValidation = () =>{
+    passwordValid= true
+    errMsg= {...errMsg}
+
+    if(password.length<8){
+        passwordValid= false
+        errMsg.password= "Password should be atleast 8 characters long!"
+    }
+    setForm({passwordValid, errMsg})
+}
+
+//repeat password Validation
+
+const repeatPassword = () =>{
+    repeatPasswordValid= true
+    errMsg={...errMsg}
+
+    if(repeatPassword!==password){
+        repeatPassword=false
+        errMsg.repeatPassword= "Sorry! Password doesn't match"       
+    }
+    setForm({repeatPasswordValid, errMsg})
+}
+
     return(
         <form>
             {/* input for username */}
