22828655fc2a835d672195ff91d77ef58c44edde
Minakshi Verma <mvedwan@gmail.com>
2020-11-25 00:41:39 -0500

    State is added to form components!

--- react-forms/src/StandardForm/Stdform.js
+++ react-forms/src/StandardForm/Stdform.js
@@ -1,33 +1,45 @@
+import {useState} from 'react';
 
 const Stdform = () =>{
+const [form, setForm] =useState({
+    username: "", userNameValid: false,
+    email:"", emailValid: false,
+    password: "", passwordValid: false,
+    repeatPassword: "", repeatPasswordValid: false,
+    formValid: false,
+    errMsg: {}
+})
+
+const {username, userNameValid, email, emailValid, password, passwordValid, repeatPassword, repeatPasswordValid, formValid, errMsg} = form
 
     return(
         <form>
             {/* input for username */}
             <div className="form-group">
                 <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" id="username" />
+                <input type="text" className="form-control" id="username" value={username} onChange={(e)=>setForm({username: e.target.value})} />
 
             </div>
 
             {/* Input for email */}
-            <div className="form-group">
+            <div className="form-group" >
                 <label htmlFor="email">Email</label>
-                <input type="text" className="form-control" id="email" />
+                <input type="text" className="form-control" id="email" value={email}
+                onChange={(e)=>setForm({email: e.target.value})}  />
                 
             </div>
 
             {/* Input for Password */}
             <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" />
+                <input type="password" className="form-control" id="password" value={password} onChange={(e)=>setForm({password: e.target.value})}/>
 
             </div>
 
             {/* Input for Repeat Password */}
             <div className="form-group">
                 <label htmlFor="repeatPassword">Repeat Password</label>
-                <input type="password" className="form-control" id="repeatPassword" />
+                <input type="password" className="form-control" id="repeatPassword" value={repeatPassword} onChange={(e)=>setForm({repeatPassword: e.target.value})} />
 
             </div>
 
